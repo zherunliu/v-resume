3fa5d8d64cc14289faefd24c5cfa3a42a450078e
tianchenghang <tiancheng.hang@bytedance.com>
2026-03-10 13:14:45 +0800

    fix: Fix base url

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/resume-vue3/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/v-resume/' : '/',
   plugins: [vue(), vueDevTools(), tailwindcss()],
   resolve: {
     alias: {
